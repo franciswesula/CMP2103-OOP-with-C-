821c724f72716dccd3898f778e6280f6f99a9396
franciswesula <franciswesula@gmail.com>
2026-09-15 09:22:05 +0300

    Complete Grading Multiple Choice Questions

---
+++ Chapter_03/grading_multiple_choice_questions.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    const int NUM_STUDENTS = 8;
+    const int NUM_QUESTIONS = 10;
+
+    char answers[NUM_STUDENTS][NUM_QUESTIONS] = {
+        {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}, // Student 0
+        {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'}, // Student 1
+        {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'}, // Student 2
+        {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'}, // Student 3
+        {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}, // Student 4
+        {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}, // Student 5
+        {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}, // Student 6
+        {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}  // Student 7
+    };
+
+    char key[NUM_QUESTIONS] = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+
+    for (int i = 0; i < NUM_STUDENTS; ++i) {
+        int correctCount = 0;
+
+        for (int j = 0; j < NUM_QUESTIONS; ++j) {
+            if (answers[i][j] == key[j]) {
+                correctCount++;
+            }
+        }
+
+        cout << "Student " << i << "'s number of correct answers is " << correctCount << "\n";
+    }
+
+    return 0;
+}
